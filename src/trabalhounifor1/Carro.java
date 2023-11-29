bfe8f9afa698c19758b50d599f9b7d6dbeda6a28
Julio-Na-Gaita <lincolnceara@yahoo.com.br>
2023-11-29 14:38:16 -0300

    Atualizando

---
+++ src/trabalhounifor1/Carro.java
@@ -0,0 +1,54 @@
+package trabalhounifor1;
+
+public class Carro {
+    private String modelo;
+    private String marca;
+    private int ano;
+    private double preco;
+
+    public Carro() {
+    }
+
+    public Carro(String modelo, String marca, int ano, double preco) {
+        this.modelo = modelo;
+        this.marca = marca;
+        this.ano = ano;
+        this.preco = preco;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public void acelerar() {
+        System.out.println("Carro acelerando...");
+    }
+}
